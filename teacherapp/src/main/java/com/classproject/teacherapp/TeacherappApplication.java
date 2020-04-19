e77e4df94dcd924ca7c45ddb64b41196384f23a6
wangshuo <1015560223@qq.com>
2020-04-19 23:04:07 +0800

    更改application

--- teacherapp/src/main/java/com/classproject/teacherapp/TeacherappApplication.java
+++ teacherapp/src/main/java/com/classproject/teacherapp/TeacherappApplication.java
@@ -3,13 +3,14 @@ package com.classproject.teacherapp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 @MapperScan("com.classproject.teacherapp.dao")
-public class TeacherappApplication {
+public class TeacherappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TeacherappApplication.class, args);
