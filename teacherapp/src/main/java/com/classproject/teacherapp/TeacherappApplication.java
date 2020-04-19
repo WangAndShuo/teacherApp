29de2e40c2e958e4fe860621a686da51301ea69b
wangshuo <1015560223@qq.com>
2020-04-19 23:20:57 +0800

    4.0

--- teacherapp/src/main/java/com/classproject/teacherapp/TeacherappApplication.java
+++ teacherapp/src/main/java/com/classproject/teacherapp/TeacherappApplication.java
@@ -3,6 +3,7 @@ package com.classproject.teacherapp;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,6 +15,11 @@ public class TeacherappApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TeacherappApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TeacherappApplication.class);
     }
 
 
